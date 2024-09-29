4396729defd02638c6ea40085fb4477bc293bedb
Nicholas Irvin Suhendi <nicholasirvin123@gmail.com>
2024-09-29 14:59:50 +0700

    feat: open new tab when clicking on links on ExperienceView & add header on ExperienceView

--- src/components/experience_view/ExperienceData.ts
+++ src/components/experience_view/ExperienceData.ts
@@ -12,13 +12,13 @@ export const ExperienceDataList: ExperienceData[] = [
         ExperienceTitle: 'Software Teaching Assistant',
         ExperienceStartDate: 'February 2023',
         ExperienceEndDate: 'January 2024',
-        ExperienceDescription: 'Teaching assistant for various IT courses (Algorithm and Programming, Database, Data mining, and lots more) at <a>Bina Nusantara University</a>.',
+        ExperienceDescription: 'Teaching assistant for various IT courses (Algorithm and Programming, Database, Data mining, and lots more) at <a target="_blank" href="https://binus.ac.id/">Bina Nusantara University</a>.',
     },
     {
         id: 'experience2',
         ExperienceTitle: 'Database Administrator',
         ExperienceStartDate: 'January 2024',
         ExperienceEndDate: 'Present',
-        ExperienceDescription: 'Maintain and manage the database for <a>Bina Nusantara University</a>. This includes optimizing the database and the jobs running on it as well as coordinating with other divisions to ensure data availability and consistency.'
+        ExperienceDescription: 'Maintain and manage the database for <a target="_blank" href="https://binus.ac.id/">Bina Nusantara University</a>. This includes optimizing the database and the jobs running on it as well as coordinating with other divisions to ensure data availability and consistency.'
     }
 ]
